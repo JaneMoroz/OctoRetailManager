08308749add2c7d2b931b43d074923d4280e06c0
JaneMoroz <moroz.jane@yahoo.com>
2021-03-11 13:48:31 +0300

    Wire up Login Form to API
    Connects the login form button  to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed.

---
+++ ORMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using ORMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace ORMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
