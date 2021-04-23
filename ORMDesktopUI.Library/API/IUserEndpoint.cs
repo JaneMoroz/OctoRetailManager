48804007aff7d758a4be0ecdc5bfab87e1c9cf2d
JaneMoroz <moroz.jane@yahoo.com>
2021-04-23 14:52:13 +0300

    Managing User Roles
    Admin can now add and remove roles from a user in user administration window.

--- ORMDesktopUI.Library/API/IUserEndpoint.cs
+++ ORMDesktopUI.Library/API/IUserEndpoint.cs
@@ -6,6 +6,9 @@ namespace ORMDesktopUI.Library.API
 {
     public interface IUserEndpoint
     {
+        Task AddUserToRole(string userId, string roleName);
         Task<List<UserModel>> GetAll();
+        Task<Dictionary<string, string>> GetAllRoles();
+        Task RemoveUserFromRole(string userId, string roleName);
     }
 }
